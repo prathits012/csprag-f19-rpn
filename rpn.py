4dab9dc58fefdc48badd32addb4ef4ecb352910d
prathit <prathit@umich.edu>
2019-10-25 14:34:43 -0400

    test working

--- rpn.py
+++ rpn.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python 3
 
 def calculate(arg):
-    arg
+    stack=list()
+    for token in arg.split():
+        if token =='+':
+            arg1=stack.pop()
+            arg2=stack.pop()
+            result = arg1+arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+        print(stack)
+    return stack.pop()
 def main():
     while True:
         calculate(input("rpn calc> "))
-    if __name__ = '__main__':
+    if __name__ == '__main__':
         main()
