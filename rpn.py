28f543996e751e9f0879f376b50a5b6570e35f93
prathit <prathit@umich.edu>
2019-11-06 17:19:18 -0500

    fixed ^

--- rpn.py
+++ rpn.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python 3
+import operator
+op = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+    "^": operator.pow,
+}
 
-def calculate(arg):
-    stack=list()
-    for token in arg.split():
-        if token =='+':
-            arg2=stack.pop()
-            arg1=stack.pop()
-            result = arg1+arg2
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = op[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
             stack.append(result)
-        if token =='-':
-            arg2=stack.pop()
-            arg1=stack.pop()
-            result = arg1-arg2
-            stack.append(result)
-        else:
-            stack.append(int(token))
         print(stack)
+    if len(stack) != 1:
+        raise TypeError("wrong args")
     return stack.pop()
 def main():
     while True:
-        calculate(input("rpn calc> "))
-    if __name__ == '__main__':
-        main()
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
+if __name__ == '__main__':
+    main()
--- test_rpn.py
+++ test_rpn.py
@@ -4,12 +4,12 @@ class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2,result)
-    def test_add(self):
+    def test_sub(self):
         result = rpn.calculate('5 3 -')
         self.assertEqual(2,result)
     def test_badinput(self):
         result = rpn.calculate('5 3 -')
         self.assertEqual(2,result)
-    def test_carat(self):
+    def test_pow(self):
         result = rpn.calculate('3 3 ^')
-        self.assertEqual(9,result)
+        self.assertEqual(27,result)
